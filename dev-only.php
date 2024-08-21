2df336f53ae1147b8233138bce4001791742bdba
Tan Nguyen <hi@tan.ng>
2024-08-21 02:36:06 +0000

    feat: prepare meta data

--- dev-only.php
+++ dev-only.php
@@ -8,6 +8,10 @@ Version: 0.0.0-alpha
 Author URI: https://tan.ng/
 */
 
+if ( ! defined( 'WP_ENVIRONMENT_TYPE' ) || WP_ENVIRONMENT_TYPE !== 'development' ) {
+	return;
+}
+
 // Functions for debugging
 function ddd( $object ) {
 	echo '<pre>';
@@ -26,3 +30,22 @@ function clv( $object ) {
 
 // Always login REST API as admin
 add_filter( 'determine_current_user', fn() => 1 );
+
+// Modify REST API respose to include all data
+add_filter( 'rest_prepare_post', function ( $response, $post, $request ) {
+	// Include all meta data
+	$meta = get_post_meta( $post->ID );
+
+	// Loop through all meta data and unserialize the value
+	foreach ( $meta as $key => $value ) {
+		foreach ( $value as $index => $val ) {
+			$value[$index] = maybe_unserialize( $val );
+		}
+
+		$meta[$key] = $value;
+	}
+
+	$response->data['_meta'] = $meta;
+
+	return $response;
+}, 10, 3 );
